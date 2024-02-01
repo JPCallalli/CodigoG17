6e83dd0047840e3e19ac7f3a28168bbb2db55f8b
Jean Callalli <jeanpl13@hotmail.com>
2024-01-31 20:48:56 -0500

    Added React

---
+++ semana9/01-react/src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css"
+// cuando importamos algo de JS o JSX, no es necesario agregarle la extensión, si es algo diferente es imperativo.
+import Mensaje from "./components/Mensaje"
+// Un componente no es mas que una función
+// El nombre de la funcion que representa el componente 
+// 1.el nombre tiene que estar en Mayusculas
+function App (){
+  const titulo = "Mi primera app";
+  //3. Esta función/componente Tiene que retornar un JSX
+  return (
+    // 4. Solamente podemos retornar un solo elemento de JSX
+    <>
+      <h1>{titulo}</h1>
+      <section>
+        <h2 className="red">Holaaaaaaaaaa</h2>
+        {/*Siempre que deseemos poner codigo de JS dentro de JSX voy a tener que abrir {} y dentro poner una expresión*/}
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non quibusdam nesciunt quisquam eum, eligendi laboriosam assumenda, nostrum maiores voluptatum maxime qui distinctio rem temporibus. Rerum eaque cupiditate dolores excepturi magni. Resultado {2 + 2}
+        </p>
+        <div>
+          <label htmlFor="idNombre">Nombre:</label>
+          <input type="text" id="idNombre" />
+        </div>
+      </section>
+      {/*Toda etiqueta ya sea de html o de un componente debe estar cerrada o con su etiqueta de cierre */}
+      <hr />
+      <Mensaje></Mensaje>
+      <Mensaje />
+      <Mensaje />
+    </>
+  )
+}
+
+// 2. exportar esta función
+export default App;
---
+++ semana9/01-react/src/components/Mensaje.jsx
@@ -0,0 +1,16 @@
+// Ya no es obligarorio en importar React en c/ componente
+// En algunas versiones de libreiras como react Native es necesario
+// import React from "react";
+const Mensaje = () => {
+    return(
+        // estas llaves de html que aparentan ser vacias
+        // se conocen como un Fragment
+        <>
+            <p>
+                Mensaje : Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque eum commodi dolor consequatur sit, non possimus veritatis quos ut molestiae eos saepe dolore soluta dolorum officia. Unde aut sapiente quae?
+            </p>
+        </>
+    )
+}
+
+export default Mensaje;
